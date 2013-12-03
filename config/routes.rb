887ff15b2d379f92992056ca851af84e7503b511
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-12-03 08:03:01 -0500

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 CappyHamlApp::Application.routes.draw do
   root to: 'welcome#index'
 
-  get "welcome/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
