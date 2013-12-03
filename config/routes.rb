271916634214ff65f352f5096277b6fe0842bec6
Quin Cogdell <quincogdell@gmail.com>
2013-12-03 11:37:33 -0500

    passing quiz tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ CappyHamlApp::Application.routes.draw do
   root to: 'welcome#index'
 
   get '/haml' => 'welcome#haml'
+  get '/quiz' => 'welcome#quiz'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
